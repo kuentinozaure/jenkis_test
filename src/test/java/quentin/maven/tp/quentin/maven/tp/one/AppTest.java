47bf54d22da77312f3fd047fb6d7c88f9a9d2632
quentin.aubry <aubryquentin700@gmail.com>
2019-10-24 12:41:48 +0200

    Maven <3

---
+++ src/test/java/quentin/maven/tp/quentin/maven/tp/one/AppTest.java
@@ -0,0 +1,38 @@
+package quentin.maven.tp.quentin.maven.tp.one;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
